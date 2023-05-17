43bf1f9489ff6666ada92ab0079109637f2471b6
MikaFoxy <anonimatobrc@gmail.com>
2023-05-17 19:38:44 -0300

    Update botinfo.js

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 module.exports = {
     name: "botinfo",
+    aliases: ["info"],
     run: async (client, message, args) => {
         message.reply({
             embeds: [
