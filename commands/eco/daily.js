fa72cb7842d1660e7f4832323c94536211c16b98
MikaFoxy <anonimatobrc@gmail.com>
2023-05-17 14:16:14 -0300

    Update daily.js

--- commands/eco/daily.js
+++ commands/eco/daily.js
@@ -4,7 +4,7 @@ module.exports = {
     run: async (client, message, args) => {
         const userdb = await client.db.findById({ _id: message.author.id });
         if (!userdb) return message.reply({ content: `Você não utilizou o comando: \n**++registrar**.` });
-        if (Date.now() < userdb.eco.timers.dailyCooldown) return message.reply({ content: `Você se encontra em modo de recarga, tente novamente ${~~(userdb.eco.timers.dailyCooldown / 1000 )}.` });
+        if (Date.now() < userdb.eco.timers.dailyCooldown) return message.reply({ content: `Você se encontra em modo de recarga, tente novamente <t:${~~(userdb.eco.timers.dailyCooldown / 1000 )}:R>.` });
         let coins = Math.floor(Math.random() * 100) + 500;
         message.reply({
             embeds: [
