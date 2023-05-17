b6f86ca528b9f978baad5e08a73784a36f355e93
MikaFoxy <anonimatobrc@gmail.com>
2023-05-17 19:41:53 -0300

    Update verify.js

--- commands/eco/verify.js
+++ commands/eco/verify.js
@@ -3,12 +3,12 @@ module.exports = {
   name: "verify",
   aliases: ["registrar", "verificar"],
   run: async (client, message, args) => {
-    const userdb = client.db.findById({ _id: message.author.id });
+    let userdb = client.db.findById({ _id: message.author.id });
     if (!userdb) {
       const create = new client.db({ _id: message.author.id });
       await create.save();
       return message.reply({ content: `${message.author}, uma conta foi criada para vc e agora já podes desfrutar dos meus comandos.` });
-    } else {
+    } else if (userdb) {
       return message.reply({ content: `${message.author}, Você já está registrado em uma conta.` });
     }
   }
