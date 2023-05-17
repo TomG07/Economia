fd30661c88b8b1e8e87466f267d1a0edc2072ed5
MikaFoxy <anonimatobrc@gmail.com>
2023-05-17 19:05:44 -0300

    Update gf.js

--- commands/eco/gf.js
+++ commands/eco/gf.js
@@ -1,11 +1,12 @@
 const Discord = require("discord.js");
 module.exports = {
     name: "gf",
+    aliases: ["amor"],
     run: async (client, message, args) => {
         const userdb = await client.db.findById({ _id: message.author.id });
         if (!userdb) return message.reply({ content: `Você não utilizou o comando: \n**++registrar**.` });
         if (Date.now() < userdb.eco.timers.gfCooldown) return message.reply({ content: `Você se encontra em modo de recarga, tente novamente ${~~(userdb.eco.timers.gfCooldown / 1000)}.` });
-        if (userdb.eco.marry.userId == null) return message.reply({ content: `Você se encontra solteiro(a), se case utilizando o comando: \n**++registrar**.` });
+        if (userdb.eco.marry.userId == null) return message.reply({ content: `Você se encontra solteiro(a), se case utilizando o comando: \n**++casar**.` });
         let coins = Math.floor(Math.random() * 150) + 300;
         message.reply({
             embeds: [
