6058e934b7bddb1fabc5566c1664e094ab3899da
mikafoxy <anonimatobrc@gmail.com>
2023-05-16 22:36:24 -0300

    teste

---
+++ commands/info/help.js
@@ -0,0 +1,7 @@
+const Discord = require("discord.js");
+module.exports = {
+    name: "help",
+    run: async (client, message, args) => {
+
+    }
+}
---
+++ events/bot/mention.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+const client = require("../../index");
+module.exports = {
+    name: Discord.Events.MessageCreate,
+    once: false,
+    async execute(message) {
+
+    }
+}
