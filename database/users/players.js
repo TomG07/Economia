7030c7010d2f14f71830bcdb332454e9f1e9c99c
mikafoxy <anonimatobrc@gmail.com>
2023-05-16 21:56:53 -0300

    mongo

---
+++ database/users/players.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+const documentSave = new Schema({
+    _id: { type: String },
+    eco: {
+        coins: { type: Number, default: 0 },
+        xp: { type: Number, default: 0 },
+        job: { type: String, default: "null" },
+        marry: {
+            userId: { type: String, default: "null" },
+            marryDate: { type: Number, default: 0 },
+        },
+        timers: {
+            dailyCooldown: { type: Number, default: 0 },
+            workCooldown: { type: Number, default: 0 },
+            gfCooldown: { type: Number, default: 0 },
+        }
+    }
+});
+module.exports = model("dbuser", documentSave);
