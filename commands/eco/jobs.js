046488b309b151b078c3ce201c65b955e3be8aae
mikafoxy <anonimatobrc@gmail.com>
2023-05-16 22:19:18 -0300

    upjob

--- commands/eco/jobs.js
+++ commands/eco/jobs.js
@@ -41,7 +41,9 @@ module.exports = {
                 if (i.customId === "jobs") {
                     let x = i.values[0];
                     if (userdb.eco.xp < x.split("-")[1]) return i.followUp({ content: `${i.user}, Você não possui **${x.split("-")[1]} de experiêcia**, continue realizando suas tarefas para conseguir mais xp.`, ephemeral: true });
+                    if (userdb.eco.job === x.split("-")[0]) return i.followUp({ content: `${i.user}, Você já se encontra nesse emprego.`, ephemeral: true });
                     i.followUp({ content: `${i.user}, Você foi contradado como **${x.split("-")[0]}** e agora seu salário é de **${x.split("-")[2]} diamantes.**`, ephemeral: false });
+                    await client.db.updateOne({ _id: message.author.id }, { $set: { "eco.job": x.split("-")[0], }, });
                 }
             });
         });
