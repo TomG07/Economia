00504bdddc25dc0702d1d5f3ea61df18d428b2c5
MikaFoxy <anonimatobrc@gmail.com>
2023-05-18 09:46:15 -0300

    Update rank.js

--- commands/eco/rank.js
+++ commands/eco/rank.js
@@ -3,18 +3,21 @@ module.exports = {
   name: "rank",
   aliases: ["ranking", "top"],
   run: async(client, message, args) => {
-     await message.sendTyping();
+     await message.channel.sendTyping();
      let top = await client.db.find({}).sort({ "eco.coins": -1 });
      let content = "";
      for (let i = 0; i <= 2; i++) {
        let user = await client.users.fetch(`${top[i]._id}`);
-       content += `\> **${i + 1}.** ${user.username}` + `\n:gem: \`${top[i].eco.coins} diamantes\`` + "\n";
+       content += `\> #**${i + 1}**: ${user.username}` + `\n:gem: \`${top[i].eco.coins} diamantes\`` + "\n";
      }
     message.reply({
        embeds: [
           new Discord.EmbedBuilder()
+           .setAuthor({ name: `${message.author.tag}`, iconURL: `${message.author.displayAvatarURL({ dynamic: true })}` })
            .setTitle("⭐ Top mais rico dod diamantes")
            .setDescription(`${content}`)
+           .setColor("#303136")
+           .setTimestamp()
        ]
     });
   }
