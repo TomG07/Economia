4a558f4b20a823a74200c02a1fccecdc2e0e0cc5
MikaFoxy <anonimatobrc@gmail.com>
2023-05-18 09:37:18 -0300

    Update rank.js

--- commands/eco/rank.js
+++ commands/eco/rank.js
@@ -5,8 +5,16 @@ module.exports = {
   run: async(client, message, args) => {
      let top = await client.db.find({}).sort({ "eco.coins": -1 });
      let content = "";
-     for (let i = 0; i <= 3; i++) {
+     for (let i = 0; i <= 2; i++) {
        let user = await client.users.fetch(`${top[i]._id}`);
+       content += `**${i + 1}.** ${user.username}\n`;
      }
+    message.reply({
+       embeds: [
+          new Discord.EmbedBuilder()
+           .setTitle("Top mais rico dod diamantes")
+           .setDescription(`${content}`)
+       ]
+    });
   }
 }
