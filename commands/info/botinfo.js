e9f294f9e08d186821b7f5abcf7e76c43d45f332
MikaFoxy <anonimatobrc@gmail.com>
2023-05-17 22:19:01 -0300

    Update botinfo.js

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -9,6 +9,22 @@ module.exports = {
                     .setTitle("Minhas informações")
                     .setThumbnail(client.user.displayAvatarURL())
                     .setColor("#303136")
+                    .addFields({
+                        name:"🛠 Creator",
+                        value: `MikaCat (Id: \`1027989059198537728\`)`
+                    }, {
+                        name: "💻 Minha versão",
+                        value: "1.0.2"
+                    }, {
+                        name: "📚 Livraria",
+                        value: "`Discord.js`"
+                    }, {
+                       name: "⚙ Prefixo",
+                       value: "`++`"
+                    }, {
+                        name: "🗂 Total de comandos",
+                        value: `${client.commands.size}`
+                    })
             ]
         });
     }
