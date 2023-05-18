4e695b0c32bbcdbfac0a1c66831a2f19ab18499d
MikaFoxy <anonimatobrc@gmail.com>
2023-05-17 21:42:50 -0300

    Update cd.js

--- commands/eco/cd.js
+++ commands/eco/cd.js
@@ -5,7 +5,16 @@ module.exports = {
   run: async(client, message, args) => {
         const userdb = await client.db.findById({ _id: message.author.id });
         if (!userdb) return message.reply({ content: `Você não utilizou o comando: \n**++registrar**.` });
-
-       
+        let timers = userdb.eco.timers;
+     message.reply({
+        embeds: [
+          new Discord.EmbedBuilder()
+           .setTitle("Tempo das suas coletas")
+           .setThumbnail(message.author.displayAvatarURL({ dunamic: true }))
+           .setTimestamp()
+           .setColor("#303136")
+           .setDescription(`**Daily**: ${Date.now() < timers.dailyCooldown ? `<t:${~~(timers.dailyCooldown / 1000)}:R>` : "✅ | Colete agora!"}`)
+        ]
+     });
   }
 }
