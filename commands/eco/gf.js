41c2626029a58f713087f5d2730714d66a33aa71
MikaFoxy <anonimatobrc@gmail.com>
2023-05-18 21:25:59 -0300

    Update gf.js

--- commands/eco/gf.js
+++ commands/eco/gf.js
@@ -5,7 +5,7 @@ module.exports = {
     run: async (client, message, args) => {
         const userdb = await client.db.findById({ _id: message.author.id });
         if (!userdb) return message.reply({ content: `Você não utilizou o comando: \n**++registrar**.` });
-        if (Date.now() < userdb.eco.timers.gfCooldown) return message.reply({ content: `Você se encontra em modo de recarga, tente novamente ${~~(userdb.eco.timers.gfCooldown / 1000)}.` });
+        if (Date.now() < userdb.eco.timers.gfCooldown) return message.reply({ content: `Você se encontra em modo de recarga, tente novamente <t:${~~(userdb.eco.timers.gfCooldown / 1000)}:R>.` });
         if (userdb.eco.marry.userId == null) return message.reply({ content: `Você se encontra solteiro(a), se case utilizando o comando: \n**++casar**.` });
         let coins = Math.floor(Math.random() * 150) + 300;
         message.reply({
