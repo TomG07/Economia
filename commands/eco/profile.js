93c9164acd9d0090223fe05d1128430c494184c7
MikaFoxy <anonimatobrc@gmail.com>
2023-05-19 09:54:51 -0300

    Update profile.js

--- commands/eco/profile.js
+++ commands/eco/profile.js
@@ -1,5 +1,8 @@
 const Discord = require("discord.js");
 const Canvas = require("canvas");
+Canvas.registerFont("assets/fonts/SourceCodePro-VariableFont_wght.ttf", {
+  family: "Source Code Pro",
+});
 module.exports = {
   name: "profile",
   aliases: ["perfil"],
@@ -16,7 +19,11 @@ module.exports = {
     //BG
      const background = await Canvas.loadImage("https://media.discordapp.net/attachments/1108841656729485393/1109096161568108574/1684499475063.png");
      context.drawImage(background, 0, 0, canvas.width, canvas.height);
-    //attachment   
+     //name
+     context.font("400 30px 'Source Code Pro'")
+     context.fillStyle("#ffffff")
+     context.fillText(member.user.username, 220, 405)
+     //attachment  
     const attachment = new Discord.AttachmentBuilder(canvas.toBuffer(), { name: "profile.png" });
     message.reply({ files: [attachment] });
   }
