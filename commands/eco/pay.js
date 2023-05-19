2b08361893416c2d658fac18df38e97f61f723ad
MikaFoxy <anonimatobrc@gmail.com>
2023-05-19 08:30:40 -0300

    Update pay.js

--- commands/eco/pay.js
+++ commands/eco/pay.js
@@ -1,12 +1,11 @@
 const Discord = require("discord.js");
 module.exports = {
   name: "pay",
-  aliases: ["pagar"],
+  aliases: ["pagar", "transferir"],
     run: async(client, message, args) => {
       let member = message.mentions.members.first();
       if (!member) return;
-      let value = args.slice(1.2).join(" ");
-      
-      
+      let value = args.slice(1).join(" ");
+      message.reply({ content: `deseja transferir **${value} diamantes**.` });
   }
 }
