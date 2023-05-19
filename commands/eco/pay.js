bbcc704d0fde52e4d9ade2c0bd6d1e394e491117
MikaFoxy <anonimatobrc@gmail.com>
2023-05-19 09:12:11 -0300

    Update pay.js

--- commands/eco/pay.js
+++ commands/eco/pay.js
@@ -9,7 +9,7 @@ module.exports = {
       if (member.user.id === message.author.id) return message.reply({ content: `Não pode pagar a si mesmo.` });
       let value = args.slice(1).join(" ");
       if (!value) return message.reply({ content: `` });
-      if (!isNaN(value)) return message.reply({ content: `Somente números devem ser considerados na quantia do pagamento.` });
+      if (isNaN(value)) return message.reply({ content: `Somente números devem ser considerados na quantia do pagamento.` });
       if (value < 100) return message.reply({ content: `Você só pode fazer pagamentos com quantias maiores que **100 diamantes**.` });
       if (value > 50000) return message.reply({ content: `Você só pode transferir quantias maiores que **50k** de uma vez só.` });
       message.reply({ content: `<:diamantes:1109086535439110164> **|** ${message.author}, deseja transferir **${value} diamantes** para sua conta.` });
