aec5f323c20fd494d6b6958ccd3fe70e32e809dd
MikaFoxy <anonimatobrc@gmail.com>
2023-05-21 14:59:50 -0300

    Update cd.js

--- commands/eco/cd.js
+++ commands/eco/cd.js
@@ -9,10 +9,10 @@ module.exports = {
       message.reply({
          embeds: [
             new Discord.EmbedBuilder()
-               .setTitle("Tempo das suas coletas")
+               .setTitle("<:FlowerPurple:1109899097655222272> Tempo das suas coletas!")
                .setThumbnail(message.author.displayAvatarURL({ dunamic: true }))
                .setTimestamp()
-               .setColor("#303136")
+               .setColor("#9b59b6")
                .setDescription(`**Daily**: ${Date.now() < timers.dailyCooldown ? `<:skipper_cooldown:1108750205202993162> <t:${~~(timers.dailyCooldown / 1000)}:R>` : "<:certo:1108750401764851752> | Colete agora!"}\n**Work**: ${Date.now() < timers.workCooldown ? `<:skipper_cooldown:1108750205202993162> <t:${~~(timers.workCooldown / 1000)}:R>` : "<:certo:1108750401764851752> | Colete agora!"}\n**GF**: ${Date.now() < timers.gfCooldown ? `<:skipper_cooldown:1108750205202993162> <t:${~~(timers.gfCooldown / 1000)}:R>` : "<:certo:1108750401764851752> | Colete agora!"}`)
          ]
       });
