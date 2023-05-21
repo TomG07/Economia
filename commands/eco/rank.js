5c1b61e536503b43f0ab9fad2119ec84cd2f04cd
MikaFoxy <anonimatobrc@gmail.com>
2023-05-21 15:38:33 -0300

    hhh

--- commands/eco/rank.js
+++ commands/eco/rank.js
@@ -8,15 +8,15 @@ module.exports = {
      let content = "";
      for (let i = 0; i <= 2; i++) {
        let user = await client.users.fetch(`${top[i]._id}`);
-       content += `\> #**${i + 1}**: ${user.username}` + `\n<:diamante:1108743116778917958> \`${top[i].eco.coins} diamantes\`` + "\n";
+       content += `\> #**${i + 1}**: ${user.username}` + `\n<:FusionCoins:1109901003572133994> \`${top[i].eco.coins} bits\`` + "\n";
      }
     message.reply({
        embeds: [
           new Discord.EmbedBuilder()
            .setAuthor({ name: `${message.author.tag}`, iconURL: `${message.author.displayAvatarURL({ dynamic: true })}` })
-           .setTitle("⭐ Top mais rico dos diamantes!")
+           .setTitle("⭐ Top mais rico dos Bits!")
            .setDescription(`${content}`)
-           .setColor("#303136")
+           .setColor("#9b59b6")
            .setTimestamp()
        ]
     });
