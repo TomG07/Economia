68aac735d43b62047656902e956132756a9b06b9
MikaFoxy <anonimatobrc@gmail.com>
2023-05-21 17:59:15 -0300

    Create interactionCreate.js

---
+++ events/guild/interactionCreate.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+const client = require("../../index")
+module.exports = {
+  name: Discord.Events.InteractionCreate,
+  once: false,
+  async execute(interaction) {
+  
+  }
+}
