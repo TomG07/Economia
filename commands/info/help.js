d15751b938ad49c62915e746d234232623f4f537
MikaFoxy <anonimatobrc@gmail.com>
2023-05-21 14:45:17 -0300

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -10,10 +10,10 @@ module.exports = {
                     .setTitle("Lista dos meus comandos")
                     .setThumbnail(client.user.displayAvatarURL())
                     .addFields({
-                        name: "💰 Economia",
+                        name: "<:FlowerPurple:1085226294683389992> Economia",
                         value: "`saldo`, `daily`, `empregos`, `cd`, `perfil`, `emblemas`, `casar`, `gf`, `divorciar`, `trabalhar`, `top`, `pagar`"
                     }, {
-                        name: "🔧 Utilidades",
+                        name: "<:FlowerPurple:1085226294683389992> Utilidades",
                         value: "`ajuda`, `botinfo`, `verificar`"
                     })
                     .setColor("#9b59b6")
