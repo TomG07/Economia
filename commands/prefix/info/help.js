031ba58549ede442d80e7fe1653def1f150a467e
MikaFoxy <anonimatobrc@gmail.com>
2023-05-24 14:03:30 -0300

    hhhhh

--- commands/prefix/info/help.js
+++ commands/prefix/info/help.js
@@ -8,7 +8,7 @@ module.exports = {
                 new Discord.EmbedBuilder()
                     .setAuthor({ name: `${message.author.tag}`, iconURL: `${message.author.displayAvatarURL({ dynamic: true })}` })
                     .setTitle("Lista dos meus comandos")
-                    .setThumbnail(client.user.displayAvatarURL())
+                    .setFooter({ text: "Anxienty todos os direitos reservados!", iconURL: `${client.user.displayAvatarURL()}` })
                     .addFields({
                         name: "<:FlowerPurple:1109899097655222272> Economia",
                         value: "`saldo`, `daily`, `empregos`, `cd`, `perfil`, `emblemas`, `casar`, `gf`, `divorciar`, `trabalhar`, `top`, `pagar`"
@@ -16,6 +16,7 @@ module.exports = {
                         name: "<:FlowerPurple:1109899097655222272> Utilidades",
                         value: "`ajuda`, `botinfo`, `verificar`"
                     })
+                    .setImage("https://media.discordapp.net/attachments/1062929961754841180/1110975912192778400/e6f35409643fd900051676d5cde22b2c.png")
                     .setColor("#9b59b6")
                     .setTimestamp()
             ]
