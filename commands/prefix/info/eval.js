88ecb92951928a07a11cf27d43dbc0ff21f8f0d9
MikaFoxy <anonimatobrc@gmail.com>
2023-05-25 13:59:13 -0300

    Create eval.js

---
+++ commands/prefix/info/eval.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+const util = require("util");
+module.exports = {
+  name: "eval",
+  aliases: ["test", "ev"],
+  run: async(client, message,args) => {
+    if (message.author.id != "1027989059198537728") return;
+    const expression = args.join(" ");
+    const resultEmbed = new Discord.EmbedBuilder();
+    const inputEmbed = new Discord.EmbedBuilder();
+    inputEmbed.setAuthor({ name: `${message.user.username}`, iconURL: `${message.author.displayAvatarURL()}` })
+    inputEmbed.setTitle("Entrada")
+    inputEmbed.setColor("#c44762");
+    inputEmbed.setFooter({ text: "Tools 🌸", iconURL: `${kaiya.user.displayAvatarURL()}` })
+    await message.guild.members.fetch();
+    inputEmbed.setDescription(`${Discord.codeBlock("js", expression)}`);
+    try {
+      const result = util.inspect(eval(expression));
+      if (result.length > 4096) {
+        resultEmbed.setTitle("Vish.. Deu errado! 😭");
+        resultEmbed.setColor("#ff6347");
+        resultEmbed.setDescription("\> O resultado ultrapassou o limite de **4096 caracteres**. *Por esse motivo não pode ser exibido.*");
+      } else {
+        resultEmbed.setTitle("📦 Saida");
+        resultEmbed.setColor("#303136");
+        resultEmbed.setDescription(`${Discord.codeBlock("js", result)}`);
+      }
+      message.reply({
+        embeds: [inputEmbed, resultEmbed]
+      });
+    } catch (err) {
+      resultEmbed.setTitle("Vish.. Algo deu errado! 😭");
+      resultEmbed.setColor("#ff6347")
+      resultEmbed.setDescription(`${Discord.codeBlock("js", err)}`);
+      message.reply({
+        embeds: [inputEmbed, resultEmbed]
+      });
+    }
+  }
+}
