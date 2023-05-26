90c71281861272c59a80a72f28eef5b006555617
MikaFoxy <anonimatobrc@gmail.com>
2023-05-26 09:49:42 -0300

    yy

--- commands/prefix/eco/crop.js
+++ commands/prefix/eco/crop.js
@@ -7,7 +7,7 @@ module.exports = {
         if (!userdb) return message.reply({ content: `Você não utilizou o comando: \n**a.registrar**.` });
         if (userdb.eco.farm.owner !== true) return message.reply({ content: `Você não possue uma fazenda, compre utilizando o comando: \n**a.loja**.` });
         let db = [];
-        userdb.eco.farm.seeds.forEach((x) => {
+        userdb.eco.farm.seeds.toArray().forEach((x) => {
             db.push(x);
         });
         message.reply({ content: `${db}` })
