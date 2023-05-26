d983424c5aaab34a898fae42162a37bdc479297a
MikaFoxy <anonimatobrc@gmail.com>
2023-05-26 13:22:36 -0300

    ufyu

--- commands/prefix/eco/crop.js
+++ commands/prefix/eco/crop.js
@@ -7,7 +7,28 @@ module.exports = {
         if (!userdb) return message.reply({ content: `Você não utilizou o comando: \n**a.registrar**.` });
         if (userdb.eco.farm.owner !== true) return message.reply({ content: `Você não possue uma fazenda, compre utilizando o comando: \n**a.loja**.` });
         message.reply({
-            content: `<:FlowerYellow:1085226520710234152> __**Sua fazenda**__!\n\n:potato: \`Platação de Batata\`: ${userdb.eco.farm.seeds.batata.count >= 1 ? `\n\> Lotes: [${userdb.eco.farm.seeds.batata.count}]\n\> Colher: ${Date.now() < userdb.eco.farm.seeds.batata.cooldown ? `<:Battery_Yellow:1089745568122818680> <t:${~~(userdb.eco.farm.seeds.batata.cooldown / 1000)}:R>` : "<:Battery_Green:1089745543963623524> | Colete agora!"}` : "Lote vazio!"}`
+            content: `<:FlowerYellow:1085226520710234152> __**Sua fazenda**__!\n\n:potato: \`Platação de Batata\`: ${userdb.eco.farm.seeds.batata.count >= 1 ? `\n\> Lotes: [${userdb.eco.farm.seeds.batata.count}]\n\> Colher: ${Date.now() < userdb.eco.farm.seeds.batata.cooldown ? `<:Battery_Yellow:1089745568122818680> <t:${~~(userdb.eco.farm.seeds.batata.cooldown / 1000)}:R>` : "<:Battery_Green:1089745543963623524> | Colete agora!"}` : "Lote vazio!"}`,
+            components: [
+                new Discord.ActionRowBuilder().addComponents(
+                    new Discord.ButtonBuilder()
+                        .setCustomId("wgetbatata")
+                        .setLabel("Colher")
+                        .setEmoji("🥔")
+                        .setStyle(Discord.ButtonStyle.Secondary)
+                        .setDisabled(userdb.eco.farm.seeds.batata.count >= 1 && Date.now() > userdb.eco.farm.seeds.batata.cooldown ? false : true)
+                )], fetchReply: true
+        }).then((int) => {
+            const coletou = int.createMessageComponentCollector({ time: 36000 });
+            coletou.on('collect', async (i) => {
+                await i.deferUpdate();
+                if (i.customId === "wgetbatata") {
+                    coletou.stop();
+                    if (i.user.id !== message.author.id) return i.followUp({ content: `Essa decisão não cabe a vc!`, ephemeral: true });
+                    //if (checar.eco.coins < value) return message.reply({ content: `Saldo insuficiente!` });
+                    int.edit({ content: `${i.user}, Você colheu **${userdb.eco.farm.seeds.batata.count} batatas** da sua fazenda.`, components: [] });
+                    // await client.db.updateOne({ _id: i.user.id }, { $inc: { "eco.coins": value, }, });
+                }
+            });
         });
     }
 }
