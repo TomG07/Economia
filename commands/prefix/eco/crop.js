a3ba896fcc0f67dfc0c5a3864a58fde0bd0ba0de
MikaFoxy <anonimatobrc@gmail.com>
2023-05-26 09:38:23 -0300

    uujkkj

---
+++ commands/prefix/eco/crop.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+module.exports = {
+    name: "crop",
+    aliases: ["colher", "safra"],
+    run: async (client, message, args) => {
+        const userdb = await client.db.findById({ _id: message.author.id });
+        if (!userdb) return message.reply({ content: `Você não utilizou o comando: \n**a.registrar**.` });
+        if (userdb.eco.farm.owner !== true) return message.reply({ content: `Você não possue uma fazenda, compre utilizando o comando: \n**a.loja**.` });
+
+    }
+}
--- commands/prefix/eco/daily.js
+++ commands/prefix/eco/daily.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 module.exports = {
     name: "daily",
+    aliases: ["coletar", "caxinha"],
     run: async (client, message, args) => {
         const userdb = await client.db.findById({ _id: message.author.id });
         if (!userdb) return message.reply({ content: `Você não utilizou o comando: \n**a.registrar**.` });
