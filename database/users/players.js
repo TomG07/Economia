78dedc93a5599127ad1f2e586e06079c7d07b804
MikaFoxy <anonimatobrc@gmail.com>
2023-05-26 07:24:32 -0300

    yytthh

--- database/users/players.js
+++ database/users/players.js
@@ -5,10 +5,45 @@ const documentSave = new Schema({
         coins: { type: Number, default: 0 },
         xp: { type: Number, default: 0 },
         job: { type: String, default: null },
-        badges: { type: Array, default: [] }, 
+        badges: { type: Array, default: [] },
         marry: {
             userId: { type: String, default: null },
             marryDate: { type: Number, default: 0 },
+        },
+        farm: {
+            owner: { type: Boolean, default: false },
+            seeds: {
+                batata: {
+                    count: { type: Number, default: 0 },
+                    max: 10,
+                    coins: 300,
+                    cooldown: { type: Number, default: 0 },
+                },
+                arroz: {
+                    count: { type: Number, default: 0 },
+                    max: 20,
+                    coins: 200,
+                    cooldown: { type: Number, default: 0 },
+                },
+                trigo: {
+                    count: { type: Number, default: 0 },
+                    max: 16,
+                    coins: 170,
+                    cooldown: { type: Number, default: 0 },
+                },
+                milho: {
+                    count: { type: Number, default: 0 },
+                    max: 15,
+                    coins: 150,
+                    cooldown: { type: Number, default: 0 },
+                },
+                cenoura: {
+                    count: { type: Number, default: 0 },
+                    max: 5,
+                    coins: 50,
+                    cooldown: { type: Number, default: 0 },
+                },
+            }
         },
         timers: {
             dailyCooldown: { type: Number, default: 0 },
