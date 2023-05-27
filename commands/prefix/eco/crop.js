624b6a3b412a1abcd50984d9992645d40ef0db03
MikaFoxy <anonimatobrc@gmail.com>
2023-05-27 14:20:01 -0300

    Update crop.js

--- commands/prefix/eco/crop.js
+++ commands/prefix/eco/crop.js
@@ -7,7 +7,7 @@ module.exports = {
         if (!userdb) return message.reply({ content: `Você não utilizou o comando: \n**a.registrar**.` });
         if (userdb.eco.farm.owner !== true) return message.reply({ content: `Você não possue uma fazenda, compre utilizando o comando: \n**a.loja**.` });
         message.reply({
-            content: `<:FlowerYellow:1085226520710234152> __**Sua fazenda**__!\n\n:potato: \`Platação de Batata\`: ${userdb.eco.farm.seeds.batata.count >= 1 ? `\n\> Lotes: [${userdb.eco.farm.seeds.batata.count}]\n\> Colher: ${Date.now() < userdb.eco.farm.seeds.batata.cooldown ? `<:Battery_Yellow:1089745568122818680> <t:${~~(userdb.eco.farm.seeds.batata.cooldown / 1000)}:R>` : "<:Battery_Green:1089745543963623524> | Colete agora!"}` : "Lote vazio!"}`,
+            content: `<:fazenda:1112066431585091654> __**Sua fazenda**__!\n\n<:plantacao:1112066775509635162> \`Platação de Batata\`: ${userdb.eco.farm.seeds.batata.count >= 1 ? `\n\> Lotes: [${userdb.eco.farm.seeds.batata.count}]\n\> Colher: ${Date.now() < userdb.eco.farm.seeds.batata.cooldown ? `<:Battery_Yellow:1089745568122818680> <t:${~~(userdb.eco.farm.seeds.batata.cooldown / 1000)}:R>` : "<:Battery_Green:1089745543963623524> | Colete agora!"}` : "Lote vazio!"}`,
             components: [
                 new Discord.ActionRowBuilder().addComponents(
                     new Discord.ButtonBuilder()
