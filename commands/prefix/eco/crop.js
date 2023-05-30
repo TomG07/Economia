439051200bd7f49c6c02f7e988cb7a51e6169b10
MikaFoxy <anonimatobrc@gmail.com>
2023-05-30 09:20:09 -0300

    hhgg

--- commands/prefix/eco/crop.js
+++ commands/prefix/eco/crop.js
@@ -7,7 +7,7 @@ module.exports = {
         if (!userdb) return message.reply({ content: `Você não utilizou o comando: \n**a.registrar**.` });
         if (userdb.eco.farm.owner !== true) return message.reply({ content: `Você não possue uma fazenda, compre utilizando o comando: \n**a.loja**.` });
         message.reply({
-            content: `<:fazenda:1112066431585091654> __**Sua fazenda**__!\n\n<:plantacao:1112066775509635162> \`Platação de Batata\`: ${userdb.eco.farm.seeds.batata.count >= 1 ? `\n\> Lotes: [${userdb.eco.farm.seeds.batata.count}]\n\> Colher: ${Date.now() < userdb.eco.farm.seeds.batata.cooldown ? `<:Battery_Yellow:1089745568122818680> <t:${~~(userdb.eco.farm.seeds.batata.cooldown / 1000)}:R>` : "<:Battery_Green:1089745543963623524> | Colete agora!"}` : "Lote vazio!"}`,
+            content: `<:fazenda:1112066431585091654> __**Sua fazenda**__!\n\n<:plantacao:1112066775509635162> \`Platação de Batata\`: ${userdb.eco.farm.seeds.batata.count >= 1 ? `\n\> Lotes: [${userdb.eco.farm.seeds.batata.count}]\n\> Colher: ${Date.now() < userdb.eco.farm.seeds.batata.cooldown ? `<:Battery_Yellow:1089745568122818680> <t:${~~(userdb.eco.farm.seeds.batata.cooldown / 1000)}:R>` : "<:Battery_Green:1089745543963623524> | Colete agora!"}` : "Lote vazio!"}\n\n<:plantacao:1112066775509635162> \`Platação de Trigo\`: ${userdb.eco.farm.seeds.trigo.count >= 1 ? `\n\> Lotes: [${userdb.eco.farm.seeds.trigo.count}]\n\> Colher: ${Date.now() < userdb.eco.farm.seeds.trigo.cooldown ? `<:Battery_Yellow:1089745568122818680> <t:${~~(userdb.eco.farm.seeds.trigo.cooldown / 1000)}:R>` : "<:Battery_Green:1089745543963623524> | Colete agora!"}` : "Lote vazio!"}\n\n<:plantacao:1112066775509635162> \`Platação de Batata\`: ${userdb.eco.farm.seeds.milho.count >= 1 ? `\n\> Lotes: [${userdb.eco.farm.seeds.milho.count}]\n\> Colher: ${Date.now() < userdb.eco.farm.seeds.milho.cooldown ? `<:Battery_Yellow:1089745568122818680> <t:${~~(userdb.eco.farm.seeds.milho.cooldown / 1000)}:R>` : "<:Battery_Green:1089745543963623524> | Colete agora!"}` : "Lote vazio!"}`,
             components: [
                 new Discord.ActionRowBuilder().addComponents(
                     new Discord.ButtonBuilder()
@@ -15,7 +15,19 @@ module.exports = {
                         .setLabel("Colher")
                         .setEmoji("🥔")
                         .setStyle(Discord.ButtonStyle.Secondary)
-                        .setDisabled(userdb.eco.farm.seeds.batata.count >= 1 && Date.now() > userdb.eco.farm.seeds.batata.cooldown ? false : true)
+                        .setDisabled(userdb.eco.farm.seeds.batata.count >= 1 && Date.now() > userdb.eco.farm.seeds.batata.cooldown ? false : true),
+                    new Discord.ButtonBuilder()
+                        .setCustomId("wgettrigo")
+                        .setLabel("Colher")
+                        .setEmoji("🌾")
+                        .setStyle(Discord.ButtonStyle.Secondary)
+                        .setDisabled(userdb.eco.farm.seeds.trigo.count >= 1 && Date.now() > userdb.eco.farm.seeds.trigo.cooldown ? false : true),
+                    new Discord.ButtonBuilder()
+                        .setCustomId("wgetmilho")
+                        .setLabel("Colher")
+                        .setEmoji("🌽")
+                        .setStyle(Discord.ButtonStyle.Secondary)
+                        .setDisabled(userdb.eco.farm.seeds.milho.count >= 1 && Date.now() > userdb.eco.farm.seeds.milho.cooldown ? false : true)
                 )], fetchReply: true
         }).then((int) => {
             const coletou = int.createMessageComponentCollector({ time: 36000 });
@@ -29,8 +41,25 @@ module.exports = {
                     if (size <= 0) return;
                     int.edit({ content: `<:1_Correto:1079943018477523004> ${i.user}, Você colheu **${size} batatas** da sua fazenda.`, components: [] });
                     await client.db.updateOne({ _id: i.user.id }, { $inc: { "eco.coins": 2, "eco.farm.seeds.batata.count": -size, }, });
+                } else if (i.customId === "wgettrigo") {
+                    coletou.stop();
+                    if (i.user.id !== message.author.id) return i.followUp({ content: `Essa decisão não cabe a vc!`, ephemeral: true });
+                    //if (checar.eco.coins < value) return message.reply({ content: `Saldo insuficiente!` });
+                    let size = userdb.eco.farm.seeds.trigo.count;
+                    if (size <= 0) return;
+                    int.edit({ content: `<:1_Correto:1079943018477523004> ${i.user}, Você colheu **${size} trigos** da sua fazenda.`, components: [] });
+                    await client.db.updateOne({ _id: i.user.id }, { $inc: { "eco.coins": 2, "eco.farm.seeds.trigo.count": -size, }, });
+                } else if (i.customId === "wgetmilho") {
+                    coletou.stop();
+                    if (i.user.id !== message.author.id) return i.followUp({ content: `Essa decisão não cabe a vc!`, ephemeral: true });
+                    //if (checar.eco.coins < value) return message.reply({ content: `Saldo insuficiente!` });
+                    let size = userdb.eco.farm.seeds.milho.count;
+                    if (size <= 0) return;
+                    int.edit({ content: `<:1_Correto:1079943018477523004> ${i.user}, Você colheu **${size} milhos** da sua fazenda.`, components: [] });
+                    await client.db.updateOne({ _id: i.user.id }, { $inc: { "eco.coins": 2, "eco.farm.seeds.milho.count": -size, }, });
                 }
             });
         });
     }
 }
+//aa 
