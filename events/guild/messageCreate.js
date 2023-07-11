ba3cd044ec9ebca1194d56bcd218f0e70f14be0b
MikaFoxy <anonimatobrc@gmail.com>
2023-07-11 13:40:06 -0300

    Update messageCreate.js

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -8,7 +8,7 @@ module.exports = {
         if (message.author.bot) return;
         // if (message.guild.id !== "1102036342172553226") return;
         if (message.channel.type == Discord.ChannelType.DM) return;
-        let prefix = "a.";
+        let prefix = "ny!";
         if (!message.content.toLowerCase().startsWith(prefix.toLowerCase())) return;
         if (!message.content.startsWith(prefix)) return;
         const args = message.content.slice(prefix.length).trim().split(/ +/g);
