4418bfd17abab1a6e5cab77e03aaee260dda55c0
MikaFoxy <anonimatobrc@gmail.com>
2023-07-11 13:34:36 -0300

    Update and rename car.js to rep.js

--- commands/prefix/eco/rep.js
+++ commands/prefix/eco/rep.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "rep",
     aliases: ["reputação", "medalha", "curtir"],
     run: async (client, message, args) => {
-     let member = message.mentions.members.first() || message.guild.members.cache.find(member => member.user.id === args[0]);;
+        let member = message.mentions.members.first() || message.guild.members.cache.find(member => member.user.id === args[0]);;
         if (!member) return message.reply({ content: `${message.author}, Você deve mencionar o usuário que vai receber essa reputação.` });
         if (member.user.bot) return message.reply({ content: `${message.author}, Bots não recebem reputações.` });
         if (member.user.id === message.author.id) return message.reply({ content: `${message.author}, Não pode dar uma reputação si mesmo.` });
@@ -11,25 +11,18 @@ module.exports = {
         if (!userdb) return message.reply({ content: `${message.author}, Você deve se registrar com o comando: \n**ny!registrar**.` });
         const twouserdb = await client.db.findOne({ _id: member.user.id });
         if (!twouserdb) return message.reply({ content: `${message.author}, Esse jogador **__${member.user.username}__** deve fazer o registro com o comando:\n**ny!registrar**.` })
+        if (Date.now() < userdb.eco.timers.repCooldown) return message.reply({ content: `⏰ **|** ${message.author},Você se encontra em modo de recarga, tente novamente <t:${~~(userdb.eco.timers.repCooldown / 1000)}:R>.` });
         message.reply({
             embeds: [
                 new Discord.EmbedBuilder()
-                    .setTitle("Corrida concluído!")
-                    .setDescription(`${message.author}, ganhou <:Stars:1111647398188564510> **${coins} bits** + <:Exp:1111648750864171154> '1XP!' de experiência ao fazer picos de motorista de aplicativo.`)
+                    .setTitle(":star: Reputação!")
+                    .setDescription(`${message.author}, você deu (+1) reputação para ${member} e agora ele(a) tem um total de **${Math.floor(twouserdb.eco.reps + 1)} reputações**.`)
                     .setColor("#303136")
-                    .setFooter({ text: `${message.author.tag}`, iconURL: `${message.author.displayAvatarURL()}` })
-            ],
-            components: [
-                new Discord.ActionRowBuilder().addComponents(
-                    new Discord.ButtonBuilder()
-                        .setCustomId("car")
-                        .setLabel("Volte em 1 hora!")
-                        .setEmoji("<:FlowerPurple:1109899097655222272>")
-                        .setStyle(Discord.ButtonStyle.Secondary)
-                        .setDisabled(true)
-                )
+                    .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+                    .setAuthor({ name: `${message.author.username}`, iconURL: `${message.author.displayAvatarURL()}` })
             ]
         });
-        await client.db.updateOne({ _id: message.author.id }, { $inc: { "eco.coins": coins, "eco.xp": 1, }, $set: { "eco.timers.uberCooldown": Date.now() + 3600000, }, });
+        await cliient.db.updateOne({ _id: member.user.id }, { $inc: { "eco.reps": 1 }, });
+        await client.db.updateOne({ _id: message.author.id }, { $set: { "eco.timers.repCooldown": Date.now() + 7600000, }, });
     }
 }
