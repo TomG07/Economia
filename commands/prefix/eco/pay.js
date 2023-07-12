bfb77014dea76068f0166fa9f5da4a22a3ea0587
MikaFoxy <anonimatobrc@gmail.com>
2023-07-12 15:12:01 -0300

    Update pay.js

--- commands/prefix/eco/pay.js
+++ commands/prefix/eco/pay.js
@@ -2,15 +2,16 @@ const Discord = require("discord.js");
 module.exports = {
   name: "pay",
   aliases: ["pagar", "transferir"],
-  run: async (client, message, args) => {
+  run: async (client, message, args, prefix) => {
+    let p = prefix || "ny!";
     let member = message.mentions.members.first() || message.guild.members.cache.find(member => member.user.id === args[0]);
     if (!member) return message.reply({ content: `${message.author}, Você deve mencionar o usuário que vai receber o pagamento.` });
     if (member.user.bot) return message.reply({ content: `${message.author}, Os bots não recebem pagamentos.` });
     if (member.user.id === message.author.id) return message.reply({ content: `${message.author}, Você não pode pagar a si mesmo.` });
     const userdb = await client.db.findOne({ _id: message.author.id });
-    if (!userdb) return message.reply({ content: `${message.author}, Você deve se registrar com o comando: \n**ny!registrar**.` });
+    if (!userdb) return message.reply({ content: `${message.author}, Você deve se registrar com o comando: \n**${p}registrar**.` });
     const twouserdb = await client.db.findOne({ _id: member.user.id });
-    if (!twouserdb) return message.reply({ content: `${message.author}, Esse jogador **__${member.user.username}__** deve fazer o registro com o comando:\n**ny!registrar**.` })
+    if (!twouserdb) return message.reply({ content: `${message.author}, Esse jogador **__${member.user.username}__** deve fazer o registro com o comando:\n**${p}registrar**.` })
     let value = args.slice(1).join(" ");
     if (!value) return message.reply({ content: `${message.author}, Você não informou a quantia do pagamento!` });
     if (isNaN(value)) return message.reply({ content: `${message.author}, Somente números devem ser considerados na quantia do pagamento.` });
