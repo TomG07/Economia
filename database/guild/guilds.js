6ebd97de49cee312d1a7198bc0768d1b64bea4aa
MikaFoxy <anonimatobrc@gmail.com>
2023-07-12 13:19:20 -0300

    Create guilds.js

---
+++ database/guild/guilds.js
@@ -0,0 +1,8 @@
+const { Schema, model } = require("mongoose");
+const documentSave = new Schema({
+     guildId: { type: String },                         
+     config: {
+       prefix: { type: String, default: "ny!" },
+     },
+});
+module.exports = model("dbuser", documentSave);
