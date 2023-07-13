0a82940aad4a98ea485aba263f53a245b735c4a6
MikaFoxy <anonimatobrc@gmail.com>
2023-07-12 21:18:57 -0300

    Update jobs.js

--- commands/prefix/eco/jobs.js
+++ commands/prefix/eco/jobs.js
@@ -3,10 +3,11 @@ const jobs = require("../../../util/jobs.json");
 module.exports = {
     name: "jobs",
     aliases: ["empregos"],
-    run: async (client, message, args) => {
-        await message.channel.sendTyping()
+    run: async (client, message, args, prrfix) => {
+        await message.channel.sendTyping();
+        let p = prefix || "ny!";
         const userdb = await client.db.findById({ _id: message.author.id });
-        if (!userdb) return message.reply({ content: `${message.author}, Você deve se registrar com o comando: \n**ny!registrar**.` });
+        if (!userdb) return message.reply({ content: `${message.author}, Você deve se registrar com o comando: \n**${p}registrar**.` });
         const empregos = Object.entries(jobs);
         message.reply({
             embeds: [
@@ -14,9 +15,9 @@ module.exports = {
                     .setAuthor({ name: `${message.author.username}`, iconURL: `${message.author.displayAvatarURL()}` })
                     .setTitle("Painel de Empregos!")
                     .setDescription(`${empregos.map(([key, value]) => {
-                        return `\> <:Inventario:1128316648223412284> **Emprego ${key}**\nSalário: \`${value.salario} euros.\`\nRequisito: \`${value.exp}XP! de experiência.\` `
+                        return `\> <:Inventario:1128316648223412284> **Emprego ${key}**\nSalário: \`${value.salario} magias.\`\nRequisito: \`${value.exp}XP! de experiência.\` `
                     }).join("\n")}`)
-                    .setColor("#303136")
+                    .setColor("#2a2d30")
                     .addFields({
                         name: `<:Emoji_GraficoSubiu:1116338129377448016> Experiência:`,
                         value: `Você tem **__${userdb.eco.xp}XP__**!`
@@ -48,10 +49,10 @@ module.exports = {
                     if (userdb.eco.job === x.split("-")[0]) return i.followUp({ content: `${i.user}, Você já se encontra nesse emprego!`, ephemeral: true });
                     if (userdb.eco.job !== null) {
                         await client.db.updateOne({ _id: i.user.id }, { $set: { "eco.job": x.split("-")[0], }, });
-                        i.followUp({ content: `${i.user}, Você foi contradado como **${x.split("-")[0]}** e agora seu salário é de **${x.split("-")[2]} euros.**`, ephemeral: false });
+                        i.followUp({ content: `${i.user}, Você foi contradado como **${x.split("-")[0]}** e agora seu salário é de **${x.split("-")[2]} magias.**`, ephemeral: false });
                     } else {
                         await client.db.updateOne({ _id: i.user.id }, { $set: { "eco.job": x.split("-")[0], }, $push: { "eco.badges": "STAFF", }, });
-                        i.followUp({ content: `${i.user}, Você foi contradado como **${x.split("-")[0]}** e agora seu salário é de **${x.split("-")[2]} euros** e você resgatou um novo emblema (<:Staff:1107072021231317193>) em seu perfil, utilize o comando:\n\`a.perfil\``, ephemeral: false });
+                        i.followUp({ content: `${i.user}, Você foi contradado como **${x.split("-")[0]}** e agora seu salário é de **${x.split("-")[2]} magias** e você resgatou um novo emblema (<:Inventario:1128316648223412284>) em seu perfil! Veja utilizando o comando:\n\`${p}perfil\``, ephemeral: false });
                     }
                 }
             });
