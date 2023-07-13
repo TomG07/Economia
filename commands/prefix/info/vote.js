8bd94bf11598b52f4d7cd41a29afc0aa8f060fbd
MikaFoxy <anonimatobrc@gmail.com>
2023-07-13 06:44:46 -0300

    Create vote.js

---
+++ commands/prefix/info/vote.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+module.exports = {
+  name: "vote",
+  aliases: ["votar", "topgg", "bl"],
+  run: async(client, message, args) => {
+    message.reply({
+      embeds: [
+        new Discord.EmbedBuilder()
+         .setAuthor({ name: `${message.author.username}`, iconURL: `${message.author.displayAvatarURL()}` })
+         .setDescription(`${message.author}, Você sabia que pode votar em mim a cada **12 horas** e receber um bônus de **1,200 magias** a cada voto que vc me der! Vote em mim [clicando aqui](https://top.gg/bot/1128306038337183795/vote).`)
+      ]
+    });
+  }
+}
