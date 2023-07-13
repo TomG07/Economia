b9a2dc691e854af07c1ab033f5a25e938b58400a
MikaFoxy <anonimatobrc@gmail.com>
2023-07-13 15:41:27 -0300

    Create addcoins.js

---
+++ commands/prefix/eco/addcoins.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+module.exports = {
+  name: "addcoins",
+  aliases: ["addmagias", "setmagias"],
+  run: async(client, message, args, prefix) => {
+    let p = prefix || "ny!";
+  }
+}
