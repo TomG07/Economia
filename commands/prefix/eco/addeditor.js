a74d1613bdab2c6169966d6633e75dc1cf4e3027
MikaFoxy <anonimatobrc@gmail.com>
2023-07-13 15:04:52 -0300

    Update addeditor.js

--- commands/prefix/eco/addeditor.js
+++ commands/prefix/eco/addeditor.js
@@ -10,6 +10,8 @@ module.exports = {
     if (member.user.bot) return message.reply({ content: `${message.author}, Os bots não podem ser adicionados aos editores!` });
     const userdb = await client.db.findOne({ userId: `${message.guild.id}-${member.user.id}` });
     if (!userdb) return message.reply({ content: `${message.author.id !== member.user.id ? `${message.author}, Esse jogador **__${member.user.username}__**` : `${message.author}, Você`} deve fazer o registro com o comando:\n**${p}registrar**.` })
-        
+    if (userdb.guild.editor) return message.reply({ content: `${message.author}, O jogador mencionado(a) já é um editor(a) desse servidor!` });
+    message.reply({ content: `✅ **|** ${message.author}, Você adicionou ${member} na lista dos editores desse servidor com sucesso.` });    
+    await client.db.updateOne({ userId: `${message.guild.id}-${member.user.id}` }, { $set: { "guild.editor": true }, });
   }
 }
