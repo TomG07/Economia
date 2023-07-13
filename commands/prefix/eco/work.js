51200bfe93590d98ff9d7e8ce8e64c23f89f6043
MikaFoxy <anonimatobrc@gmail.com>
2023-07-13 14:22:50 -0300

    Update work.js

--- commands/prefix/eco/work.js
+++ commands/prefix/eco/work.js
@@ -5,7 +5,7 @@ module.exports = {
     aliases: ["trabalhar", "trampo"],
     run: async (client, message, args, prefix) => {
         let p = prefix || "ny!";
-        const userdb = await client.db.findOne({ _id: message.author.id });
+        const userdb = await client.db.findOne({ userId: `${message.guild.id}-${message.author.id}` });
         if (!userdb) return message.reply({ content: `${message.author}, Você deve se registrar com o comando: \n**${p}registrar**.` });
         if (Date.now() < userdb.eco.timers.workCooldown) return message.reply({ content: `⏰ **|** ${message.author}, Você se encontra em modo de recarga, tente novamente <t:${~~(userdb.eco.timers.workCooldown / 1000)}:R>.` });
         if (userdb.eco.job == null) return message.reply({ content: `${message.author}, Você não tem um **emprego**! Para ser contratado utilize o comando: \n**${p}empregos**.` })
