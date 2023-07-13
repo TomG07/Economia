43ae4ab3c1ab1caba370ba1d97f937992bc8a424
MikaFoxy <anonimatobrc@gmail.com>
2023-07-13 14:30:09 -0300

    Create guildMemberRemove.js

---
+++ events/guild/guildMemberRemove.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+const client = require("../../index");
+module.exports = {
+    name: Discord.Events.GuildMemberRemove,
+    once: false,
+    async execute(member) {
+      const data = await client.db.findOne({ userId: `${member.guild.id}-${member.user.id}` });
+      if (!data) return;
+      //await client.db
+    }
+}
