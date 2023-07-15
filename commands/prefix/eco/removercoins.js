63e12f43914008aa768699ad73763439e49b2277
MikaFoxy <anonimatobrc@gmail.com>
2023-07-15 10:00:51 -0300

    Update removercoins.js

--- commands/prefix/eco/removercoins.js
+++ commands/prefix/eco/removercoins.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 module.exports = {
   name: "removercoins",
-  aliases: ["removmagias", "rmvmagias"],
+  aliases: ["removermagias", "rmvmagias"],
   run: async(client, message, args, prefix) => {
     let p = prefix || "ny!";
     let member = message.mentions.members.first() || message.guild.members.cache.find(member => member.user.id === args[0]);
