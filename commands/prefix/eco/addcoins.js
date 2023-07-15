9a993fb075e9cce41d42406b3cb6c17d0463fd27
MikaFoxy <anonimatobrc@gmail.com>
2023-07-15 09:22:50 -0300

    Update addcoins.js

--- commands/prefix/eco/addcoins.js
+++ commands/prefix/eco/addcoins.js
@@ -4,5 +4,11 @@ module.exports = {
   aliases: ["addmagias", "setmagias"],
   run: async(client, message, args, prefix) => {
     let p = prefix || "ny!";
+    let member = message.mentions.members.first() || message.guild.members.cache.find(member => member.user.id === args[0]);
+    if (!member) return message.reply({ content: `${message.author}, Você não informou quem será editado!` });
+    if (member.user.bot) return message.reply({ content: `${message.author}, Os bots não podem ser editados!`});
+    const userdb = await client.db.findOne({ userId: `${message.guild.id}-${member.user.id}` });
+    if (!userdb) return message.reply({ content: `${message.author.id !== member.user.id ? `${message.author}, Esse jogador **__${member.user.username}__**` : `${message.author}, Você`} deve fazer o registro com o comando:\n**${p}registrar**.` })
+        
   }
 }
