5ff06bc85af7e53912388056b2b2bff3d3a58bf8
MikaFoxy <anonimatobrc@gmail.com>
2023-07-15 14:58:08 -0300

    hhhhh

--- commands/prefix/info/vote.js
+++ commands/prefix/info/vote.js
@@ -2,13 +2,13 @@ const Discord = require("discord.js");
 module.exports = {
   name: "vote",
   aliases: ["votar", "topgg", "bl"],
-  run: async(client, message, args) => {
+  run: async (client, message, args) => {
     message.reply({
       embeds: [
         new Discord.EmbedBuilder()
-         .setAuthor({ name: `${message.author.username}`, iconURL: `${message.author.displayAvatarURL()}` })
-         .setDescription(`${message.author}, Você sabia que pode votar em mim a cada **12 horas** e receber um bônus de **1,200 magias** a cada voto que vc me der! Vote em mim [clicando aqui](https://top.gg/bot/1128306038337183795/vote).`)
-         .setColor("#ffb6c1")
+          .setAuthor({ name: `${message.author.username}`, iconURL: `${message.author.displayAvatarURL()}` })
+          .setDescription(`${message.author}, Você sabia que pode votar em mim a cada **12 horas** e receber um bônus de **1,200 magias** a cada voto que vc me der! Vote em mim [clicando aqui](https://top.gg/bot/1128306038337183795/vote).`)
+          .setColor("#ffb6c1")
       ]
     });
   }
