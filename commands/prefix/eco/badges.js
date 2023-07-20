4196c4b31ebc852e9709ac8f7c9fe4d3c0cda88b
MikaFoxy <anonimatobrc@gmail.com>
2023-07-20 12:09:13 -0300

    kkkkk

--- commands/prefix/eco/badges.js
+++ commands/prefix/eco/badges.js
@@ -11,7 +11,7 @@ module.exports = {
           .setTimestamp()
           .setColor("#ffb6c1")
           .setFooter({ text: "Caçadores de Insignas!", iconURL: `${client.user.displayAvatarURL()}` })
-          .setDescription(`- <:anel:1119285525984063509> - Recebe essa se casando com um usuário.\n- <:Inventario:1128316648223412284> - Receba essa ao escolher seu primeiro emprego.\n- <:Fazenda:1118670191509913780> - Receba essa comprando uma fazenda.`)
+          .setDescription(`- <:money:1119274556352385046> - Recebe essa sendo um dos editores do servidor.\n- <:anel:1119285525984063509> - Recebe essa se casando com um usuário.\n- <:Inventario:1128316648223412284> - Receba essa ao escolher seu primeiro emprego.\n- <:Fazenda:1118670191509913780> - Receba essa comprando uma fazenda.`)
           .addFields({
             name: "Reservadas:",
             value: `<:developeractivo:1131556095685570691> - Somente para os meus desenvolvedores.`
